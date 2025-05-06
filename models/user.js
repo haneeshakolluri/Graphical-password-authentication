81833be19129d5b62cdfff5733b12a6629fa3651
Haneesha Kolluri <151764166+haneeshakolluri@users.noreply.github.com>
2025-05-06 21:07:51 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import mongooseUniqueValidator from "mongoose-unique-validator";
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: {type: String, required: true, minlength: 8 },
+    pattern: { type: [String], required: true },
+    sequence: { type: Boolean, required: true },
+    sets: {type: [[Object]], required: true},
+    failedAttempts: { type: Number, default: 0 }
+})
+
+userSchema.plugin(mongooseUniqueValidator)
+
+const usertModel = mongoose.model('User', userSchema)
+
+export { usertModel } 
